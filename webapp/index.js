c6e9670519f6876c2d35462efd2bec36fc504ecb
KCsenge <csengekormoczy@gmail.com>
2022-09-29 15:13:28 +0200

    No Data Binding

---
+++ webapp/index.js
@@ -0,0 +1,7 @@
+sap.ui.require(["sap/m/Text"], function (Text) {
+  "use strict";
+
+  sap.ui.getCore().attachInit(function () {
+    new Text({ text: "Hi, my name is Harry Hawk" }).placeAt("content");
+  });
+});
