26a64b9798481cdd209464bf1f12720ba4d6d4c9
KCsenge <csengekormoczy@gmail.com>
2022-09-29 16:19:33 +0200

    Creating a Model

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,14 @@
-sap.ui.require(["sap/m/Text"], function (Text) {
-  "use strict";
+sap.ui.require(
+  ["sap/m/Text", "sap/ui/model/json/JSONModel"],
+  function (Text, JSONModel) {
+    "use strict";
 
-  sap.ui.getCore().attachInit(function () {
-    new Text({ text: "Hi, my name is Harry Hawk" }).placeAt("content");
-  });
-});
+    sap.ui.getCore().attachInit(function () {
+      var oModel = new JSONModel({ greetingTest: "Hi, my name is Harry Hawk" });
+
+      sap.ui.getCore().setModel(oModel);
+
+      new Text({ text: "Hi, my name is Harry Hawk" }).placeAt("content");
+    });
+  }
+);
