f7134ed6aa74b0e703dfc055ef1db50b515026df
KCsenge <csengekormoczy@gmail.com>
2022-09-29 16:39:02 +0200

    Two-Way Data Binding

--- webapp/index.js
+++ webapp/index.js
@@ -1,14 +1,19 @@
 sap.ui.require(
-  ["sap/m/Text", "sap/ui/model/json/JSONModel"],
-  function (Text, JSONModel) {
+  ["sap/ui/core/mvc/XMLView", "sap/ui/model/json/JSONModel"],
+  function (XMLView, JSONModel) {
     "use strict";
 
     sap.ui.getCore().attachInit(function () {
-      var oModel = new JSONModel({ greetingText: "Hi, my name is Harry Hawk" });
+      var oModel = new JSONModel({
+        firstname: "Harry",
+        lastName: "Hawk",
+        enabled: true,
+        panelHeaderText: "Data Binding Basics",
+      });
 
       sap.ui.getCore().setModel(oModel);
 
-      new Text({ text: "{/greetingText}" }).placeAt("content");
+      new XMLView({ viewName: "sap.ui.demo.db.view.App" }).placeAt("content");
     });
   }
 );
